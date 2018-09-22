6001e3fee2c9bb28db99b932a1f35dd182cf9227
raulubaldi01 <raulubaldi01@outlook.it>
2018-09-22 15:12:12 +0200

    Modifiche

---
+++ LezioniTpsit/Console1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //write();
+            //Console.Writeline("Hello Word");
+            Task t = new Task(write);
+            t.Start();
+
+
+            int i = 100;
+            while (i > 0)
+            {
+                Console.Write("Raul");
+                i = i - 1;
+            }
+            Console.ReadLine();
+        }
+        public static void write()
+        {
+            int i = 100;
+            while (i > 0)
+            {
+                i--;
+                Console.Write(i);
+            }
+        }
+    }
+}
